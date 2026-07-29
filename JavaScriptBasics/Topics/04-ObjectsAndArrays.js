410eb369648ffd03356fcdd4d11163174ce0a1be
Barwy <barwy@live.com>
2026-07-29 23:00:58 +0200

    Notes on further lessons in javaScript.

--- JavaScriptBasics/Topics/04-ObjectsAndArrays.js
+++ JavaScriptBasics/Topics/04-ObjectsAndArrays.js
@@ -1,4 +1,4 @@
-//Objects can hold multiple values at once.
+//Objects can hold multiple values at once (key-value paired).
 let customer = {
 name: 'Jane',
 surname: 'Doe' //last value does not have a , at the end.
@@ -7,3 +7,26 @@ surname: 'Doe' //last value does not have a , at the end.
 //Printing one of values - there are 2 ways:
 console.log(customer.name);
 console.log(customer['surname']);
+
+//We can reassign value inside an object
+customer.name = 'George';
+customer['surname'] = 'Montana';
+console.log(customer.name);
+console.log(customer.surname);
+console.log(`${customer.name} ${customer.surname}`);
+
+//Array is a list of items, each is assigned an index number starting from 0.
+let fruits = ['banana', 'apple', 'pear'];
+console.log(fruits[1]);
+
+fruits[1] = 'peach';
+console.log(fruits[1]);
+
+//Objects can contain arrays
+let person = {
+    name: 'Adam',
+    surname: 'Smiles',
+    favouriteColors: ['black', 'blue']
+}
+
+console.log(person.favouriteColors[1]);
---
+++ JavaScriptBasics/Topics/05-Operators-RalationalAndEquality.js
@@ -0,0 +1,11 @@
+//Relational (comparison) operators - we use them to compare values on their right and left side.
+//x > y; x < y;- x is more than y and less than y.
+// x >= y; x <= y;- x is higher or equal to y and lower or equal to y.
+let value = 3 > 1
+console.log(value); //returns true.
+
+//Equality operators
+let x = 1;
+console.log(x == '1'); //true - lose comparison, it does not check data type of the value. 
+console.log(x === '1'); //false - strict comparison, it checks data type as well, as its value.
+
